85ca87aa4e6ca7e76ca587dbf2aba69c75a2620d
alec <alec.hulce@gmail.com>
2016-06-01 19:20:36 -0700

    healthcheck explanation

--- bin/healthcheck_live.sh
+++ bin/healthcheck_live.sh
@@ -11,9 +11,14 @@ if [ "$SITE" == "" ]; then
 fi
 
 hc=`curl -m5 -sS $SITE`
-if [ "`echo \"$hc\" | grep -m1 ' class=\"postteaser'`" == "" ]; then
-	echo "failed to find html snippet on homepage: ' class=\"postteaser'"
+if [ "`echo \"$hc\" | grep -m1 '<body '`" == "" ]; then
+	echo "failed to find snippet on homepage: '<body '"
 	exit 1
 fi
+# replace the above with something that checks to make sure the site is working before making backup, e.g.:
+#if [ "`echo \"$hc\" | grep -m1 ' class=\"postteaser'`" == "" ]; then
+#	echo "failed to find html snippet on homepage: ' class=\"postteaser'"
+#	exit 1
+#fi
 
 echo "OK"
